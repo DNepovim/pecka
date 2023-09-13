ff31ff3fb60bf18601872616ff69af9b7e809eb0
Dominik Bláha <jsem@dominikblaha.cz>
2023-09-13 21:03:22 +0200

    Remove redirect

--- gatsby-node.js
+++
@@ -1,8 +0,0 @@
-exports.createPages = async ({ graphql, actions }) => {
-  const { createRedirect } = actions;
-
-  createRedirect({
-    fromPath: `/prihlasovani`,
-    toPath: `https://akce.e-cirkev.cz/register/51`,
-  });
-};
